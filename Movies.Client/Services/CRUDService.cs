d71e8a01de143a77f8b304f2a12783243b96ccf5
msg4edson <msg.edson@gmail.com>
2022-02-10 10:53:24 -0300

    Fixes #6

--- Movies.Client/Services/CRUDService.cs
+++ Movies.Client/Services/CRUDService.cs
@@ -1,15 +1,48 @@
-﻿using System;
+﻿using Movies.Client.Models;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Movies.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public CRUDService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0,30);
+           
+        }
         public async Task Run()
         {
+            await GetResource();                    
+        }
         
+        public async Task GetResource()
+        {
+            var response = await _httpClient.GetAsync("api/movies");
+            response.EnsureSuccessStatusCode();
+            var movies = new List<Movie>();
+            var content = await response.Content.ReadAsStringAsync();
+            if(response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+                movies = JsonSerializer.Deserialize<List<Movie>>(content,
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+            }
+            else if (response.Content.Headers.ContentType.MediaType == "application/xml")
+            {
+                var serializer = new XmlSerializer(typeof(List<Movie>));
+                movies = (List<Movie>)serializer.Deserialize(new StringReader(content));
+            }
         }
     }
 }
